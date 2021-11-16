3265e3bafa6c1fe7b82dc56bbac75f699417d5f0
Szymon Szczerbiński <szymonszcz@student.agh.edu.pl>
2021-11-16 12:34:10 +0100

    Added another solution for zadanie_1 (lab_6)

--- laboratorium_6/zadanie_1v2.py
+++ laboratorium_6/zadanie_1v2.py
@@ -1,4 +1,6 @@
-import math
+class DeltaTooSmall(Exception):
+    pass
+
 
 a = float(input('Wprowadz A: '))
 b = float(input('Wprowadz B: '))
@@ -8,8 +10,11 @@ c = float(input('Wprowadz C: '))
 try:
     delta = (b ** 2) - (4 * a * c)
 
-    x1 = (-1 * b + math.sqrt(delta)) / (2 * a)
-    x2 = (-1 * b - math.sqrt(delta)) / (2 * a)
+    if delta < 0:
+        raise DeltaTooSmall
+
+    x1 = (-1 * b + (delta ** (1 / 2))) / (2 * a)
+    x2 = (-1 * b - (delta ** (1 / 2))) / (2 * a)
 
     if x1 == x2:
         print('Rozwiązaniem jest x: ', x1)
@@ -17,5 +22,5 @@ try:
     else:
         print('Rozwiązaniami są x1: ', x1, ', x2: ', x2)
 
-except ValueError:
+except DeltaTooSmall:
     print('Liczby zespolone nie są obsługiwane')
