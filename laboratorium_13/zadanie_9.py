1902970b9d3f0a9a5de070ed208a794bd763fcf3
Szymon Szczerbiński <szymonszcz@student.agh.edu.pl>
2022-01-08 00:54:03 +0100

    Added zadanie_9.py to laboratorium_13

---
+++ laboratorium_13/zadanie_9.py
@@ -0,0 +1,44 @@
+import copy
+
+
+def xd(n, result, output, curr_sum, start):
+    if curr_sum == n:
+        result.append(copy.copy(output))
+
+    for i in range(start, n):
+        temp_sum = curr_sum + i
+
+        if temp_sum <= n:
+            output.append(i)
+
+            xd(n, result, output, temp_sum, i)
+
+            output.pop()
+
+        else:
+            return
+
+
+def print_res(result):
+    for res in result:
+        text = ""
+        for element in res:
+            text += f" + {element}"
+        print(text[3:])
+
+
+def main():
+    n = int(input("Podaj liczbę naturalną do rozkładu: "))
+    if n == 1:
+        print(1)
+    else:
+        result = []
+        output = []
+
+        xd(n, result, output, 0, 1)
+
+        print_res(result)
+
+
+if __name__ == "__main__":
+    main()
